0eeb9a1cf54a160ccc0d53ab95158a289971fab5
kavidu717 <dushmanthakavidu143@gmail.com>
2026-02-22 00:16:04 +0530

    install the CORS and ssetup up configuration

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,13 @@
 import express from 'express'
+import cors from 'cors';
 
 
 
 const app = express()
 const port = 8000
+
+app.use(cors())
+app.use(express.json())
 
 app.get('/hello', (req, res) => {
   res.send('Hello World!')
