4592af3fac3e70b6132f84a976ac4d803ef96ebf
kavidu717 <dushmanthakavidu143@gmail.com>
2026-03-17 17:09:07 +0530

    write the task controller

--- backend/src/controllers/taskController.js
+++ backend/src/controllers/taskController.js
@@ -9,15 +9,150 @@ import mongoose from "mongoose";
 
 
 const getTask=asyncHandler(async(req,res)=>{
-    // test
+      
+    const {projectId}=req.params
+    const project=await Project.findById(projectId)
+     if(!project){
+        throw new ApiError(404,"project not found")
+     }
+    const tasks=await Task.find({project:new mongoose.Types.ObjectId(projectId)
+
+    }).populate("assignedTo","avatar fullName username")
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200,
+        tasks,
+        "tasks fetched successfully"))
+    
+  
 })
 
 const createTask=asyncHandler(async(req,res)=>{
-    // test
+    
+    const {title,description,assignedTo,status}=req.body
+   const {projectId}=req.params
+
+   const project=await Project.findById(projectId)
+   if(!project){
+    throw new ApiError(404,"project not found")
+   }
+   const files=req.files|| []
+    const attachments=files.map((file)=>{
+       return{
+        url:`${req.protocol}://${req.get("host")}/images/${file.orginalname}`,
+        mimetype:file.mimetype,
+        size:file.size
+       }
+   })
+
+    const task=await Task.create({
+        title,
+        description,
+        project:new mongoose.Types.ObjectId(projectId),
+        assignedTo:assignedTo?new mongoose.Types.ObjectId(assignedTo):undefined,
+        status,
+        assignedBy:new mongoose.Types.ObjectId(req.user._id),
+        attachments
+    })
+    return res
+    .status(201)
+    .json(new ApiResponse(201,
+        task,
+        "task created successfully"))
 })
 
 const getTaskById=asyncHandler(async(req,res)=>{
-    // test
+    
+   const {taskId}=req.params
+   const task=await Task.aggregate([
+       
+    {
+        $match: {
+            _id:new mongoose.Types.ObjectId(taskId)
+        }
+    },
+    {
+        $lookup: {
+            from: "users",
+            localField: "assignedTo",
+            foreignField: "_id",
+            as: "assignedTo",
+            pipeline: [
+                {
+                   
+                        _id:1,
+                        username:1,
+                        fullName:1,
+                        avatar:1
+                    
+                }
+            ]
+        }
+    },
+    {
+      $lookup: {
+          from: "subtasks",
+          localField: "_id",
+          foreignField: "task",
+          as: "subtasks",
+          pipeline: [
+              
+            {
+              $lookup: {
+                  from: "users",
+                  localField: "createdBy",
+                  foreignField: "_id",
+                  as: "createdBy",
+                  pipeline: [
+                    {
+                        $project: {
+                            _id:1,
+                            username:1,
+                            fullName:1,
+                            avatar:1
+                        }
+                    }
+                  ]
+                  
+              }  
+            },
+            {
+                $addFields: {
+                    createdBy:{
+                        $arrayElemAt:["$createdBy",0]
+                    }
+                }
+            }
+
+          ]
+      }  
+    },
+    {
+        $addFields: {
+            assignedTo:{
+                $arrayElemAt:["$assignedTo"]
+            }
+        }
+    }
+   
+
+
+
+   ])
+   if(!task|| task.length===0){
+    throw new ApiError(404,"task not found")
+   }
+    return res
+    .status(200)
+    .json(new ApiResponse(200,
+        task[0],
+        "task fetched successfully"))
+   
+
+
+
+ 
 })
 
 const updateTask=asyncHandler(async(req,res)=>{
