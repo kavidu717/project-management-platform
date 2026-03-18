081b24b412a52180f9d34ad018f250230af518c1
kavidu717 <dushmanthakavidu143@gmail.com>
2026-03-18 14:13:17 +0530

    create the basic page structure and pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import AppRoutes from "./routes/AppRoutes";
 
-  return (
-    <>
-      <h1>hello</h1>
-    </>
-  )
+export default function App() {
+  return <AppRoutes />;
 }
-
-export default App
---
+++ frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,25 @@
+import { Routes,Route } from "react-router-dom";
+import Login from "../pages/Login.jsx";
+import Register from "../pages/Register.jsx";
+import Dashboard from "../pages/Dashboard.jsx";
+import Projects from "../pages/Projects.jsx";
+import ProjectDetails from "../pages/ProjectDetails.jsx";
+import Tasks from "../pages/Tasks.jsx";
+import Profile from "../pages/Profile.jsx";
+import NotFound from "../pages/NotFound.jsx";
+
+
+export default function AppRoutes() {
+    return(
+        <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/projects/:id" element={<ProjectDetails />} />
+      <Route path="/tasks" element={<Tasks />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="*" element={<NotFound />} />
+        </Routes>
+    )
+}
